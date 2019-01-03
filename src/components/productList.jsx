8c33e2aaa688c7b2af206c1570d95643cc9ec2c0
Akash S <emailtoakash@gmail.com>
2019-01-03 21:51:52 +0200

    adding bootstrap and some empty files in components and container folders for team to start

---
+++ src/components/productList.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+class ProductList extends Component {
+  state = {};
+  render() {
+    return <h1>List all products here</h1>;
+  }
+}
+
+export default ProductList;
