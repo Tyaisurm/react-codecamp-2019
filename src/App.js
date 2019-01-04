2bda3a2405d5c3317443265a3dc5f528aab191c5
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2019-01-05 01:54:22 +0200

    added fluctuating prices according to the demand, no cart implementation yet

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ const product = [
   {
     id: 1,
     name: "Karhu",
+    startprice: 5.00,
     description:
       "a pale lager with a strong taste. Karhu meaning bear in Finnish"
   },
   {
     id: 2,
+    startprice: 5.40,
     name: "Sandels",
     description:
       "named after the Swedish marshal and war hero of the Finnish War, Johan August Sandels."
   },
   {
     id: 3,
+    startprice: 6.20,
     name: "Saimaa Brew",
     description: "locally brewed in Saimaa region"
   },
   {
     id: 4,
+    startprice: 4.00,
     name: "Indian Pale Ale",
     description: "hoppy beer style, brewed from pale malt "
   },
   {
     id: 5,
+    startprice: 4.50,
     name: "Kingfisher",
     description: "popular indian beer "
   }
