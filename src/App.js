5d7e6233d0850a7060260fb0c6b5e95b9e230fe4
Akash S <emailtoakash@gmail.com>
2019-01-04 14:36:13 +0200

    Less products on main page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ const product = [
   { id: 1, name: "Karu" },
   { id: 2, name: "Sandels" },
   { id: 3, name: "Saimaa Brew" },
-  { id: 4, name: "Indian Ale" },
-  { id: 5, name: "Indian Ale" },
-  { id: 6, name: "Indian Ale" },
-  { id: 7, name: "Indian Ale" },
-  { id: 8, name: "Indian Ale" },
-  { id: 9, name: "Indian Ale" },
-  { id: 10, name: "Indian Ale" }
+  { id: 4, name: "Indian Ale" }
 ];
 class App extends Component {
   render() {
