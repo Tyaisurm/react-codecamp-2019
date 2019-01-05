df8b6d9442a23c96d3df1dc47e51572b5c52593b
Elias Vähäjylkkä <vahajylkkaelias@gmail.com>
2019-01-05 15:15:49 +0200

    base for basket / cart / whatever system. The prices don't change immediately, but are added to the total basket count which whow in the 'Proceed to Order' button

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import Navigation from "./containers/navigation";
 import ProductsList from "./components/productsList";
 import { product } from "./components/constants";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      basketTotal: 0
+    }
+    this.addBasket = this.addBasket.bind(this);
+  }
+
+  addBasket(amount) {
+    this.setState({
+      basketTotal: this.state.basketTotal + amount
+    })
+  }
+
   render() {
     return (
       <div>
-        <Navigation />
-        <ProductsList product={product} />
+        <Navigation total={this.state.basketTotal}/>
+        <ProductsList product={product} addBasket={this.addBasket} />
       </div>
     );
   }
