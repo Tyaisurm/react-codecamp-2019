3fb36158a74a4e967231b59ee34714dcf6618038
Jan Librowski <janlibrowski@gmail.com>
2019-01-06 15:46:36 +0100

    set boundaries for beer count and for maximal and minimal price

--- src/App.jsx
+++ src/App.jsx
@@ -1,107 +1,115 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 import Navigation from "./containers/Navigation";
 import ProductsList from "./components/ProductsList";
-import { products } from "./fake/constants";
+import {products} from "./fake/constants";
+
+const MAX_PRICE = 20;
+const MIN_PRICE = 1;
+
 class App extends Component {
-  state = {
-      basketSum: 0,
-      drinksCount: 0,
-      bill: 0,
-      products: products.map(p => {
-          p.count = 0;
-          p.minPrice = p.maxPrice = p.price;
-          return p;
-      })
+    state = {
+        basketSum: 0,
+        drinksCount: 0,
+        bill: 0,
+        products: products.map(p => {
+            p.count = 0;
+            p.minPrice = p.maxPrice = p.price;
+            return p;
+        })
     };
 
-  render() {
-      return (
-          <div>
-              <Navigation
-                  totalDrinks={this.state.drinksCount} // sum all counts
-                  totalSum={this.state.basketSum}
-                  bill={this.state.bill}
-                  checkOut={this.checkOut}
-              />
-              <ProductsList
-                  products={this.state.products}
-                  addToBasket={this.addToBasket}
-                  removeFromBasket={this.removeFromBasket}
-              />
-          </div>
-      );
-  }
+    render() {
+        return (
+            <div>
+                <Navigation
+                    totalDrinks={this.state.drinksCount} // sum all counts
+                    totalSum={this.state.basketSum}
+                    bill={this.state.bill}
+                    checkOut={this.checkOut}
+                />
+                <ProductsList
+                    products={this.state.products}
+                    addToBasket={this.addToBasket}
+                    removeFromBasket={this.removeFromBasket}
+                />
+            </div>
+        );
+    }
 
-  checkOut = () => {
-    this.updatePrices();
-    this.updateBasket();
-    this.setState({
-      bill: (parseFloat(this.state.bill) + parseFloat(this.state.basketSum)), // Otherwise it treats it as string for some reason
-      basketDrinks: 0,
-      basketSum: 0
-    })
-  }
-
-  updatePrices() {
-      const resetProducts = products.map(p => {
-        p.price = p.count !== 0 // javascript ternary operator
-          ? 1.005 ** p.count * p.price // do it if price is not equal to 0
-          : 0.98 * p.price; // do otherwise if it is
-        p.count = 0;
-        if (p.price > p.maxPrice) {
-          p.maxPrice = p.price;
-        } else {
-          p.minPrice = p.price;
-        }
-        return p;
-      });
-
-      this.setState({
-          products: resetProducts
-      });
-  }
-
-  addToBasket = (productId) => {
-    this.setState({
-        products: this.state.products.map(p => {
-          if(p.id === productId) {
-            p.count ++;
-          }
-          return p;
+    checkOut = () => {
+        this.updatePrices();
+        this.updateBasket();
+        this.setState({
+            bill: (parseFloat(this.state.bill) + parseFloat(this.state.basketSum)), // Otherwise it treats it as string for some reason
+            basketDrinks: 0,
+            basketSum: 0
         })
-    });
+    }
 
-    this.updateBasket();
-  }
+    updatePrices() {
+        const resetProducts = products.map(p => {
+            p.price = p.count !== 0 // javascript ternary operator
+                ? 1.005 ** p.count * p.price // do it if price is not equal to 0
+                : 0.98 * p.price; // do otherwise if it is
+            if (p.price > MAX_PRICE)
+                p.price = MAX_PRICE;
+            if (p.price < MIN_PRICE)
+                p.price = MIN_PRICE;
+            p.count = 0;
+            if (p.price > p.maxPrice) {
+                p.maxPrice = p.price;
+            } else {
+                p.minPrice = p.price;
+            }
+            return p;
+        });
 
-  removeFromBasket = (productId) => {
-      this.setState({
-          products: this.state.products.map(p => {
-              if(p.id === productId && p.count > 0) {
-                  p.count --;
-              }
-              return p;
-          })
-      });
+        this.setState({
+            products: resetProducts
+        });
+    }
 
-      this.updateBasket();
-  }
+    addToBasket = (productId) => {
+        this.setState({
+            products: this.state.products.map(p => {
+                if (p.id === productId) {
+                    p.count++;
+                }
+                return p;
+            })
+        });
 
-  updateBasket = () => {
-      const allDrinks = this.state.products
-          .map(p => p.count) // map products to their counts
-          .reduce((sumOfCounts, currentCount) => sumOfCounts + currentCount);
+        this.updateBasket();
+    }
 
-      const sumPrice = this.state.products
-          .map(p => p.price * p.count) // map products to their counts times prices
-          .reduce((sumOfPrices, currentPrice) => sumOfPrices + currentPrice);
+    removeFromBasket = (productId) => {
+        this.setState({
+            products: this.state.products.map(p => {
+                if (p.id === productId && p.count > 0) {
+                    p.count--;
+                }
+                return p;
+            })
+        });
 
-      this.setState({
-          drinksCount: allDrinks,
-          basketSum: sumPrice,
-      });
-  }
+        this.updateBasket();
+    }
+
+    updateBasket = () => {
+        const allDrinks = this.state.products
+            .map(p => p.count) // map products to their counts
+            .reduce((sumOfCounts, currentCount) => sumOfCounts + currentCount);
+
+        const sumPrice = this.state.products
+            .map(p => p.price * p.count) // map products to their counts times prices
+            .reduce((sumOfPrices, currentPrice) => sumOfPrices + currentPrice);
+
+        this.setState({
+            drinksCount: allDrinks,
+            basketSum: sumPrice,
+        });
+    }
 }
 
 export default App;
--- src/containers/Navigation.jsx
+++ src/containers/Navigation.jsx
@@ -1,59 +1,64 @@
 import React from "react";
 
-class Navigation extends React.Component {
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">
-          <img
-            className="logo"
-            src={require("../logo.png")}
-            alt="Logo"
-            width="60"
-            height="60"
-          />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+const MAX_COUNT = 50;
+const MIN_COUNT = 1;
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Browse Drinks <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item active">
-              <p className="nav-link" style={{ fontWeight: "bold" }}>
-                Your Total bill: $ {this.props.bill.toFixed(2)}
-              </p>
-            </li>
-          </ul>
-          <div style={{ margin: 10, fontWeight: "bold" }}>
-            Cart : $ {this.props.totalSum.toFixed(2)}
-          </div>
-          <button
-            type="button"
-            className="btn btn-secondary float-sm-right"
-            onClick={this.props.checkOut}
-          >
-            Proceed to Order{" "}
-            <span className="badge badge-light">{this.props.totalDrinks}</span>
-            <span className="sr-only">unread messages</span>
-          </button>
-        </div>
-      </nav>
-    );
-  }
+class Navigation extends React.Component {
+    render() {
+        const {totalDrinks, totalSum, bill, checkOut} = this.props;
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">
+                    <img
+                        className="logo"
+                        src={require("../logo.png")}
+                        alt="Logo"
+                        width="60"
+                        height="60"
+                    />
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"/>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">
+                                Browse Drinks <span className="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item active">
+                            <p className="nav-link" style={{fontWeight: "bold"}}>
+                                Your Total bill: $ {bill.toFixed(2)}
+                            </p>
+                        </li>
+                    </ul>
+                    <div style={{margin: 10, fontWeight: "bold"}}>
+                        Cart : $ {totalSum.toFixed(2)}
+                    </div>
+                    <button
+                        type="button"
+                        className="btn btn-secondary float-sm-right"
+                        onClick={checkOut}
+                        disabled={(totalDrinks <= MAX_COUNT && totalDrinks >= MIN_COUNT) ? false : true}
+                    >
+                        Proceed to Order{" "}
+                        <span className="badge badge-light">{totalDrinks}</span>
+                        <span className="sr-only">unread messages</span>
+                    </button>
+                </div>
+            </nav>
+        );
+    }
 
 }
 
