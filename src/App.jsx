37bb626dfcb9c39be33bfcf1ed300b4f256cf4c0
Turtana <eliasvahis@luukku.com>
2019-01-06 17:22:48 +0200

    adjust market change values

--- src/App.jsx
+++ src/App.jsx
@@ -50,8 +50,8 @@ class App extends Component {
     updatePrices() {
         const resetProducts = products.map(p => {
             p.price = p.count !== 0 // javascript ternary operator
-                ? 1.005 ** p.count * p.price // do it if price is not equal to 0
-                : 0.98 * p.price; // do otherwise if it is
+                ? 1.02 ** p.count * p.price // do it if price is not equal to 0
+                : 0.995 * p.price; // do otherwise if it is
             if (p.price > MAX_PRICE)
                 p.price = MAX_PRICE;
             if (p.price < MIN_PRICE)
