d068b13dfdc375c09ce11855c2515a29cdadbfa5
Jaakko Tuuri <jaakko.tuuri@windowslive.com>
2019-01-06 18:02:50 +0200

    axios added to main project with npm, mockupserver upped, module updated

---
+++ mockupserver/index.js
@@ -0,0 +1,60 @@
+const jsonServer = require('json-server')
+const fs = require('fs-extra')
+const server = jsonServer.create()
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'mockupdb.json'))
+const serverport  = 3001
+
+let middlewares = jsonServer.defaults({static: "./staticfiles"})
+if(false){// use if want to make real time changes to file during run
+middlewares = [
+  ...jsonServer.defaults({ static: "./staticfiles" }),
+  ...[
+    (req, res, next) => fs
+      .readJson(path.join(__dirname, 'mockupdb.json'))
+      .then(contents => {
+        console.log("Hold up! Updating database file...");
+        router.db.assign(contents).write();
+        next();
+      })
+  ]
+];
+}
+
+router.render = (req, res) => {
+  console.log("Status code was: ",res.statusCode);
+  switch(res.statusCode){
+    case 500:
+    res.status(500).jsonp({
+      error: "500 - Internal Server Error"
+    })
+  break;
+  case 404:
+  res.status(404).jsonp({
+    error: "404 - Not Found"
+  })
+  break;
+  case 401:
+  res.status(401).jsonp({
+    error: "401 - Forbidden"
+  })
+  break;
+  default:
+  res.jsonp(res.locals.data);
+}
+}
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+//server.use('/api', router)
+//server.use(jsonServer.bodyParser)
+
+var routes = JSON.parse(fs.readFileSync(path.join(__dirname, './mockuproutes.json')));
+server.use(jsonServer.rewriter(routes));
+
+ //Use default router
+server.use('/api',router)
+server.listen(serverport, () => {
+  console.log('JSON Server is running in localhost port ',serverport)
+})
--- src/thirdparty/mockup-connection.js
+++ src/thirdparty/mockup-connection.js
@@ -1,41 +1,168 @@
 const axios = require("axios");
+const MAX_PRICE = 20;
+const MIN_PRICE = 1;
 
-/*
 
-THIS IS STILL NOT WORKIN! DO NOT USE!
-
-*/
 
 //fetch("http://localhost:3001/api/drinks/1", {"headers":{"Content-Type":"application/json"},"body":'{"description":"IVE BEEN EDITED"}',"method":"PATCH"});
-function getPosts() {
-  const getPosts = axios.get("https://jsonplaceholder.typicode.com/posts");
-  const getUsers = axios.get("https://jsonplaceholder.typicode.com/users");
 
-  const getBoth = Promise.all([getPosts, getUsers]);
-
-  return getBoth.then(function(response) {
-    const posts = response[0].data;
-    const users = response[1].data;
-
-    return posts.map(function addUserData(post) {
-      return Object.assign({}, post, {
-        userData: users.find(function findUser(user) {
-          return user.id === post.userId;
-        })
-      });
-    });
+function getAllDrinks(){
+  return axios
+  .get("http://localhost:3001/api/drinks")
+  .then(function(response) {
+    return response.data;
+  });
+}
+function getBevarages(){
+  return axios.get("http://localhost:3001/api/bevarages")
+  .then(function(response) {
+    return response.data;
+  });
+}
+function getMixers(){
+  return axios.get("http://localhost:3001/api/mixers")
+  .then(function(response) {
+    return response.data;
+  });
+}
+function getSpirits(){
+  return axios.get("http://localhost:3001/api/spirits")
+  .then(function(response) {
+    return response.data;
+  });
+}
+function getFullHistory(){
+  return axios.get("http://localhost:3001/api/history")
+  .then(function(response) {
+    return response.data;
   });
 }
 
-function getCommentsForPost(postId) {
+function getDrinkById(drinkId = -1){
+  let checkid = Number.parseInt(drinkId);
+  if(Number.isNaN(checkid)){
+    checkid = -1;
+  }
+  const address = "http://localhost:3001/api/drinks/".concat(checkid.toString())
+  return axios.get(address)
+  .then(function(response) {
+    return response.data;
+  });
+}
+
+function getHistoryById(historyId = -1){
+  let checkid = Number.parseInt(historyId);
+  if(Number.isNaN(checkid)){
+    checkid = -1;
+  }
+  const address = "http://localhost:3001/api/history/".concat(checkid.toString())
+  return axios.get(address)
+  .then(function(response) {
+    return response.data;
+  });
+}
+/*
+const getAll = axios.get("http://localhost:8001/api/drinks");
+  const getBevarages = axios.get("http://localhost:8001/api/bevarages");
+  const getMixers = axios.get("http://localhost:8001/api/mixers");
+  const getSpirits = axios.get("http://localhost:8001/api/spirits");
+  const getFullHistory = axios.get("http://localhost:8001/api/history");
+
+  function getDrinkById(drinkId){
+    axios.get("http://localhost:8001/api/drinks/");
+  }
+*/
+
+//const getAll = axios.get("http://localhost:8001/api/drinks");
+//const getBevarages = axios.get("http://localhost:8001/api/bevarages");
+//const getMixers = axios.get("http://localhost:8001/api/mixers");
+//const getSpirits = axios.get("http://localhost:8001/api/spirits");
+//const getFullHistory = axios.get("http://localhost:8001/api/history");
+
+/*
+
+updatePrices() {
+        const resetProducts = products.map(p => {
+            p.price = p.count !== 0 // javascript ternary operator
+                ? 1.02 ** p.count * p.price // do it if price is not equal to 0
+                : 0.995 * p.price; // do otherwise if it is
+            if (p.price > MAX_PRICE)
+                p.price = MAX_PRICE;
+            if (p.price < MIN_PRICE)
+                p.price = MIN_PRICE;
+            p.count = 0;
+            if (p.price > p.maxPrice) {
+                p.maxPrice = p.price;
+            } else {
+                p.minPrice = p.price;
+            }
+            return p;
+        });
+
+        this.setState({
+            products: resetProducts
+        });
+    }
+
+*/
+
+function createTransaction(itemid = -1, itemcount = 0){
+  return getFullHistory().then(getresponse => {
+    //got history of everything in response, now need to manipulate prices
+    let newprice
+    let oldprice
+    for(let k = 0;k<getresponse.length;k++){
+     switch(k){
+      case itemid:
+        oldprice = getresponse[k].data[getresponse[k].data.length-1]
+        newprice = getresponse[k].data[getresponse[k].data.length-1] * Math.pow(1.02,itemcount)
+        if (newprice > MAX_PRICE){
+          newprice = MAX_PRICE;}
+        if (newprice < MIN_PRICE){
+          newprice = MIN_PRICE;}
+        getresponse[k].data.push(newprice)
+        break;
+      case -1:
+      break;
+      default:
+        let otherprice = getresponse[k].data[getresponse[k].data.length-1] * 0.995
+        if (otherprice > MAX_PRICE){
+          otherprice = MAX_PRICE;}
+        if (otherprice < MIN_PRICE){
+          otherprice = MIN_PRICE;}
+        getresponse[k].data.push(otherprice)
+     } 
+    }
+    const transactionobj = {
+      "timestamp": new Date().toISOString(),
+      "item": itemid,
+      "amount":itemcount,
+      "price-per-unit": oldprice
+    }
+    return axios.post("http://localhost:3001/api/transactions",transactionobj).then(transactionresp =>{sendPatch(getresponse);return transactionresp.data});
+  });
+}
+
+function sendPatch(response = []){
+  if(response.length>0){
+  const latest = response.pop();
+  const dataobj = {"data":latest.data}
   return axios
-    .get("https://jsonplaceholder.typicode.com/comments?postId=" + postId)
-    .then(function(response) {
-      return response.data;
+    .patch("http://localhost:3001/api/history/".concat(latest.id.toString()), dataobj)
+    .then(function(putresponse){
+      return sendPatch(response);
     });
+  }
+  return;
 }
 
 module.exports = {
-  getPosts,
-  getCommentsForPost
+  getAllDrinks,
+  getBevarages,
+  getMixers,
+  getSpirits,
+  getFullHistory,
+  getHistoryById,
+  getDrinkById,
+  createTransaction
 };
