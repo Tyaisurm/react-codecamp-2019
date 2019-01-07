56fb3b16518fbac6476d50949b3227f45bd7770e
Akash Singhal <emailtoakash@gmail.com>
2019-01-07 14:06:40 +0200

    Adding beer quantity in Litre

--- src/fake/constants.js
+++ src/fake/constants.js
@@ -1,7 +1,7 @@
 export const products = [
     {
         id: 1,
-        name: "Karhu",
+        name: "Karhu (0.5L)",
         price: 5.0,
         description:
             "a pale lager with a strong taste. Karhu meaning bear in Finnish"
@@ -9,26 +9,26 @@ export const products = [
     {
         id: 2,
         price: 5.4,
-        name: "Sandels",
+        name: "Sandels (0.5L)",
         description:
             "named after the Swedish marshal and war hero of the Finnish War, Johan August Sandels."
     },
     {
         id: 3,
         price: 6.2,
-        name: "Saimaa Brew",
+        name: "Saimaa Brew (0.5L)",
         description: "locally brewed in Saimaa region"
     },
     {
         id: 4,
         price: 4.0,
-        name: "Indian Pale Ale",
+        name: "Indian Pale Ale (0.33L)",
         description: "hoppy beer style, brewed from pale malt "
     },
     {
         id: 5,
         price: 4.5,
-        name: "Kingfisher",
+        name: "Kingfisher (0.33L)",
         description: "popular indian beer "
     }
 ];
